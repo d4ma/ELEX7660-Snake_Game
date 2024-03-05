4992c6ef2f3beb586882cbf7646f725f5149276f
Bryce Adam <bnadam4@gmail.com>
2024-03-05 14:39:19 -0800

    Added input and output logic

--- MatrixDisplay.sv
+++ MatrixDisplay.sv
@@ -2,3 +2,14 @@
 // Output signals to the Matrix display
 // Bryce Adam
 // Mar 05, 2024
+
+module MatrixDisplay
+(
+    input logic clk,
+    input logic [15:0][15:0] grid,
+
+    // Signals to output to the daisy chained matrices
+    output logic DIN, CS, LED_CLK
+);
+
+endmodule
