d906252c39bf3fdad1967698191439165dd10564
d4ma <49056111+d4ma@users.noreply.github.com>
2024-03-19 13:26:46 -0700

    edited irReceiver module to send received code as an output

--- irReceiver.sv
+++ irReceiver.sv
@@ -1,7 +1,7 @@
 
 module irReceiver (
     input logic ir_signal,
-    output logic [1:0] direction,
+    output logic [31:0] word,
     input logic nec_clk
 );
 
@@ -11,22 +11,11 @@ module irReceiver (
 	logic [31:0] count = 0;
 	logic [31:0] buffer = 0;
 	logic [5:0] bit_count = 0;
-	logic [31:0] decoded_word = 0;
 
-	parameter [31:0] UP = 32'h20DF6A95;
-	parameter [31:0] DOWN = 32'h20DFEA15;
-	parameter [31:0] LEFT = 32'h20DF1AE5;
-	parameter [31:0] RIGHT = 32'h20DF9A65;
-	
-	always_comb begin
-		case(decoded_word)
-			UP : direction <= 0;
-			DOWN : direction <= 1;
-			LEFT : direction <= 2;
-			RIGHT : direction <= 3;
-			default : direction <= 0;
-		endcase
-	end
+	// parameter [31:0] UP = 32'h20DF6A95;
+	// parameter [31:0] DOWN = 32'h20DFEA15;
+	// parameter [31:0] LEFT = 32'h20DF1AE5;
+	// parameter [31:0] RIGHT = 32'h20DF9A65;
 
   always_ff @(posedge nec_clk) begin
 		if(~prev_ir_signal & ir_signal & ~receiving) begin
@@ -45,7 +34,7 @@ module irReceiver (
 					receive_bits <= 0;
 					receiving <= 0;
 					bit_count <= 0;
-					decoded_word <= buffer;
+					word <= buffer;
 				end
 
 				if(prev_ir_signal & ~ir_signal) begin
