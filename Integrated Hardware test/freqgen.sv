1e50fcaea5a513ae2f984db603c91fa085679f45
d4ma <49056111+d4ma@users.noreply.github.com>
2024-04-02 11:36:23 -0700

    added updated freqgen to project

---
+++ Integrated Hardware test/freqgen.sv
@@ -0,0 +1,34 @@
+// File: freqgen.sv
+// Description: toggles the nec_clk output at a desired frequency
+// Author: Kento Sasaki 
+// Date: 2024-02-03
+
+module freqgen #(
+    parameter FCLK
+)  // clock frequency, Hz
+(
+    output logic out_clk,  // speaker output
+    input logic reset_n,
+    input logic [31:0] freq,
+    input logic clk
+);  // reset and clock
+
+  logic [31:0] countup;
+
+  always @(posedge clk) begin
+
+    if (~reset_n) begin
+      out_clk <= 0;
+      countup <= 0;
+    end else begin
+      countup <= countup + (freq << 1);
+      if (countup >= FCLK-(freq << 1)) begin
+        countup <= 0;
+        out_clk <= ~out_clk;
+      end
+    end
+
+  end
+
+endmodule
+
