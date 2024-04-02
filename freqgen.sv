00c7e908922cbc4ecd3a254deb6bd8a3f240b990
d4ma <49056111+d4ma@users.noreply.github.com>
2024-04-02 10:42:44 -0700

    fixed freqgen.sv from fixed frequency to variable frequency

---
+++ freqgen.sv
@@ -0,0 +1,34 @@
+// File: freqgen.sv
+// Description: toggles the nec_clk output at a desired frequency
+// Author: Kento Sasaki 
+// Date: 2024-02-03
+
+module ir_freqgen #(
+    parameter FCLK
+)  // clock frequency, Hz
+(
+    output logic nec_clk,  // speaker output
+    input logic reset_n,
+    input logic [31:0] freq,
+    clk
+);  // reset and clock
+
+  logic [31:0] countup;
+
+  always @(posedge clk) begin
+
+    if (~reset_n) begin
+      nec_clk <= 0;
+      countup <= 0;
+    end else begin
+      countup <= countup + (freq << 1);
+      if (countup >= FCLK-(freq << 1)) begin
+        countup <= 0;
+        nec_clk <= ~nec_clk;
+      end
+    end
+
+  end
+
+endmodule
+
