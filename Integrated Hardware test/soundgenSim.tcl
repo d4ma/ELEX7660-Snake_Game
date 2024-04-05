2fdd0ac46e002be5ef68cc5bd1b460bf2bc2ad86
Bryce Adam <bnadam4@gmail.com>
2024-04-05 12:22:42 -0700

    Removed Integrated Hardware test folder

--- Integrated Hardware test/soundgenSim.tcl
+++
@@ -1,8 +0,0 @@
-set testbench soundgen_tb
-vsim -gui work.$testbench 
-add wave -r sim:/$testbench/*
-run -all
-property wave -radix unsigned /$testbench/dut_0/freq
-property wave -radix unsigned /$testbench/dut_0/count
-property wave -radix unsigned /$testbench/dut_0/notes
-property wave -radix unsigned /$testbench/dut_0/freqgen_0/countup
